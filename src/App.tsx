7c1af77048334cd00048649449c22edfbe4f82ba
raistmere <raistmere@gmail.com>
2024-01-25 15:11:51 -0600

    Added and completed mobile ui
    * Designed mobile ui based on design images
    * Added input boxes for age field
    * Added html for age text
    * Added responsive design from mobile to desktop for the divider/arrow
    * Added and used correct font and color styleguide

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import './App.css'
+import arrowIcon from "./assets/images/icon-arrow.svg";
 
 function App() {
+  const years : number = 30;
+  const months : number = 10;
+  const days : number = 26;
 
   return (
     <>
-      Day
+      <div className='wrapper'>
+        <div className="calculatorBox">
+          <div className="ageInputBox">
+            <div className="inputBox">
+              <label htmlFor="dayInput">DAY</label>
+              <input type="number" id='dayInput' name='dayInput' />
+            </div>
+            <div className="inputBox">
+              <label htmlFor="monthInput">MONTH</label>
+              <input type="number" id='monthInput' name='monthInput' />
+            </div>
+            <div className="inputBox">
+              <label htmlFor="yearInput">YEAR</label>
+              <input type="number" id='yearInput' name='yearInput' />
+            </div>
+          </div>
+          <div className="divider"></div>
+          <img className= "arrowIcon" src={arrowIcon} alt=" A Downward Arrow inside a circle" />
+          <div className="ageTextBox">
+            <p>
+              <span>{years}</span> years
+            </p>
+            <p>
+              <span>{months}</span> months
+            </p>
+            <p>
+              <span>{days}</span> days
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Day
       DD
 
       Month
@@ -17,7 +52,7 @@ function App() {
       -- months
       -- days
       
-      {/* <div class="attribution">
+      <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
       </div> */}
--- style-guide.md
+++ style-guide.md
@@ -33,19 +33,4 @@ The designs were created to the following widths:
 - Family: [Poppins](https://fonts.google.com/specimen/Poppins)
 - Weights: 400i, 700, 800i
 
-# Avoid accidental upload of the Sketch and Figma design files
-#####################################################
-## Please do not remove lines 5 and 6 - thanks! 🙂 ##
-#####################################################
-*.sketch
-*.fig
 
-# Avoid accidental XD upload if you convert the design file
-###############################################
-## Please do not remove line 12 - thanks! 🙂 ##
-###############################################
-*.xd
-
-# Avoid your project being littered with annoying .DS_Store files!
-.DS_Store
-.prettierignore
